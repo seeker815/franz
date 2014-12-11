d19b064c4dd6bdc442e4e230ec41c9b46af9abb3
Sean Clemmer <sczizzo@gmail.com>
2014-12-11 12:04:22 -0800

    Only normalize "path" if given

--- lib/franz/output.rb
+++ lib/franz/output.rb
@@ -56,21 +56,24 @@ module Franz
         until @stop
           event = opts[:input].shift
 
-          event[:path] = event[:path].sub('/home/denimuser/seam-builds/rel', '')
-          event[:path] = event[:path].sub('/home/denimuser/seam-builds/live', '')
-          event[:path] = event[:path].sub('/home/denimuser/seam-builds/beta', '')
-          event[:path] = event[:path].sub('/home/denimuser/builds/rel', '')
-          event[:path] = event[:path].sub('/home/denimuser/builds/live', '')
-          event[:path] = event[:path].sub('/home/denimuser/builds/beta', '')
-          event[:path] = event[:path].sub('/home/denimuser/cobalt-builds/rel', '')
-          event[:path] = event[:path].sub('/home/denimuser/cobalt-builds/live', '')
-          event[:path] = event[:path].sub('/home/denimuser/cobalt-builds/beta', '')
-          event[:path] = event[:path].sub('/home/denimuser/rivet-builds', '')
-          event[:path] = event[:path].sub('/home/denimuser/denim/logs', '')
-          event[:path] = event[:path].sub('/home/denimuser/seam/logs', '')
-          event[:path] = event[:path].sub('/home/denimuser/rivet/bjn/logs', '')
-          event[:path] = event[:path].sub('/home/denimuser', '')
-          event[:path] = event[:path].sub('/var/log', '')
+          if event[:path]
+            event[:path].sub! '/home/denimuser/seam-builds/rel', ''
+            event[:path].sub! '/home/denimuser/seam-builds/live', ''
+            event[:path].sub! '/home/denimuser/seam-builds/beta', ''
+            event[:path].sub! '/home/denimuser/builds/rel', ''
+            event[:path].sub! '/home/denimuser/builds/live', ''
+            event[:path].sub! '/home/denimuser/builds/beta', ''
+            event[:path].sub! '/home/denimuser/cobalt-builds/rel', ''
+            event[:path].sub! '/home/denimuser/cobalt-builds/live', ''
+            event[:path].sub! '/home/denimuser/cobalt-builds/beta', ''
+            event[:path].sub! '/home/denimuser/rivet-builds', ''
+            event[:path].sub! '/home/denimuser/denim/logs', ''
+            event[:path].sub! '/home/denimuser/seam/logs', ''
+            event[:path].sub! '/home/denimuser/rivet/bjn/logs', ''
+            event[:path].sub! '/home/denimuser', ''
+            event[:path].sub! '/var/log/franz', ''
+            event[:path].sub! '/var/log', ''
+          end
 
           log.trace \
             event: 'publish',
